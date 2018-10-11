d2c835641928cd156a1579235c6ee443dbdd8452
Zeeshan Akhlaq <21707955@bucks.ac.uk>
2018-10-11 11:23:04 +0100

    Added laptop model

---
+++ Laptops LOL/Models/Laptop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GamingLaptopWebsite.Models
+{
+    public class Laptop
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        public int LaptopID { get; set; }
+
+        [StringLength(50), Required]
+
+        public string Model { get; set; }
+
+        [StringLength(50), Required]
+
+        public string ScreenSize { get; set; }
+
+        [StringLength(6)]
+
+        public string ImagePath { get; set; }
+
+        [StringLength(128), DataType(DataType.Url)]
+
+        public string Description { get; set; }
+
+        [Required, Range(500, 3000)]
+
+        public decimal RamSize { get; set; }
+
+        public decimal StorageSize { get; set; }
+
+        public string OSVersion { get; set; }
+
+        public decimal LaptopDimensions { get; set; }
+
+        public string Price { get; set; }
+    }
+}
